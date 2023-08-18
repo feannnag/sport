a4cd49a8cac5bc26c7d116529734115149eb6b4a
feannnag <130681087+feannnag@users.noreply.github.com>
2023-08-19 01:54:16 +0300

    Update MainActivity2.kt
    Фикс подключения к модулю

--- app/src/main/java/com/example/sport/MainActivity2.kt
+++ app/src/main/java/com/example/sport/MainActivity2.kt
@@ -5,11 +5,13 @@ import android.bluetooth.BluetoothManager
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import com.example.sport.StatMsg.Companion.CONNECTING
@@ -27,6 +29,9 @@ class MainActivity2 : AppCompatActivity(), ReceiveThread.ListenerData {
     private var bAdapter: BluetoothAdapter? = null
     private lateinit var btConnection: BtConnection
     private lateinit var statusConnect: StatMsg
+    private lateinit var ibReconnection: Button
+    private lateinit var tvResult: TextView
+    private lateinit var tvStatus: TextView
 
     private fun initBtAdapter() {
         val bManager = this.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
@@ -36,6 +41,9 @@ class MainActivity2 : AppCompatActivity(), ReceiveThread.ListenerData {
 
     private fun initView() {
         tView = findViewById(R.id.textView)
+        tvStatus = findViewById(R.id.textView)
+        ibReconnection = findViewById(R.id.connectButt)
+        statusConnect = StatMsg(context = this, tvStatus = tvStatus, btStart = ibReconnection)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,18 +52,17 @@ class MainActivity2 : AppCompatActivity(), ReceiveThread.ListenerData {
 
         initView()
         initBtAdapter()
-        preferences =
-            this.getSharedPreferences(BluetoothConstants.PREFERENCES, Context.MODE_PRIVATE)
 
+        preferences = this.getSharedPreferences(BluetoothConstants.PREFERENCES, Context.MODE_PRIVATE)
         tView.text = intent.getStringExtra("height")
         val mSetting = getSharedPreferences(SettingConst.FILENAME, MODE_PRIVATE)
         Toast.makeText(this, mSetting.getString(SettingConst.HEIGHT, ""), Toast.LENGTH_SHORT).show()
 
         val goToBlSettings = findViewById<Button>(R.id.btBluetooth)
-        goToBlSettings.setOnClickListener { startActivity(Intent(this, BaseActivity::class.java)) }
+        goToBlSettings.setOnClickListener {startActivity(Intent(this, BaseActivity::class.java))}
+        ibReconnection.setOnClickListener {connectToDevice(); ibReconnection.isEnabled = true}
+
     }
-
-
 
     override fun onReceive(message: String) {
         runOnUiThread {
@@ -69,6 +76,20 @@ class MainActivity2 : AppCompatActivity(), ReceiveThread.ListenerData {
             }
 
         }
+    }
+    private fun connectToDevice() {
+        val mac = preferences?.getString(BluetoothConstants.MAC, null)
+        Log.d("Debugging", "Test Preferences = $mac")
+        if (mac != null) {
+            btConnection.connect(mac)
+            ibReconnection.isEnabled = true
+        } else {
+            tvStatus.text = "No device"
+            tvStatus.setTextColor(Color.RED)
+        }
+        val myToast = Toast.makeText(this, "MAC: $mac", Toast.LENGTH_SHORT)
+        myToast.show()
+        ibReconnection.isEnabled = true
     }
 }
 
